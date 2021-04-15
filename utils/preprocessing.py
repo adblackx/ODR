ef4d7483cc8d50104e6678b8b2680c2f5e60304e
Gad3lan <alan.adamiak@gmail.com>
2021-04-15 17:49:12 +0200

    Possibilite choix fonction prepro et barre de chargement

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -35,9 +35,9 @@ def crop(image):
 	filter_arr = []
 	for s in sums:
 			if s == 0:
-					filter_arr.append(False)
+				filter_arr.append(False)
 			else:
-					filter_arr.append(True)
+				filter_arr.append(True)
 	image = image[:, filter_arr]
 	
 	# Crop to a square shape
@@ -45,61 +45,60 @@ def crop(image):
 	w = image.shape[1]    
 	
 	if h < w:
-			x = (w - h)//2
+			x = (w - h) // 2
 			image = image[:, x:x+h, :]        
 	elif h > w:
-			x = (h - w)//2
+			x = (h - w) // 2
 			image = image[x:x+w, :, :]           
 	else:
 			pass
 	
-	image=Image.fromarray (image)
+	image = Image.fromarray (image)
 	return image
 
 def resize(image, IMG_SIZE = 512):
-	image=np.asarray (image)
+	image = np.asarray(image)
 	norm_img = np.zeros(image.shape)
 	# normalisation 0 ou 1
-	norm_img = cv2.normalize(image,  norm_img, 0, 255, cv2.NORM_MINMAX)
+	norm_img = cv2.normalize(image, norm_img, 0, 255, cv2.NORM_MINMAX)
 
 	# redimension de l'image pour avoir meme dimension entre toutes les images (à cause image resolution differentes)
 	# et conversion de la couleur de l'image car par defaut cv2 lit image en couleur bleue
 	# dans le resize, le ration = 1 par defaut car hauteur pixel = largeur pixel
 	image = cv2.resize(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), (IMG_SIZE, IMG_SIZE))
 
-	image = Image.fromarray (image)
+	image = Image.fromarray(image)
 	return image
 
-def scaleRadius (img, scale):
-	toto=int(img.shape[0]/2)
-	x=img[toto,:,:].sum (1)
-	r=(x>x.mean()/10).sum()/2
-	s=scale * 1.0/r
-	return cv2.resize (img,(0,0), fx=s , fy=s )
+def scaleRadius(img, scale):
+	radius = int(img.shape[0] / 2)
+	x = img[radius, :, :].sum(1)
+	r = (x > x.mean() / 10).sum() / 2
+	s = scale * 1.0 / r
+	return cv2.resize(img, (0, 0), fx=s , fy=s )
 
 # implementation pseudo-code Ben Graham
-def graham (image, scale = 320):
-		
+def graham(image, scale = 320):
 	image = np.asarray (image)
 	
 	# enleve les bordures
 	# #image = preprocess_image_crop(norm_img)
 
 	#redimensionnement de l'image à un rayon donné
-	image=scaleRadius (image, scale)
+	image = scaleRadius(image, scale)
 	
 	#on soustrait la couleur moyenne pour la mapper sur 50% de gris de façon à mieux faire ressortir les constrastes
-	image=cv2.addWeighted (image ,4,cv2.GaussianBlur (image,(0,0),scale/30),-4 ,128)
+	image = cv2.addWeighted(image, 4, cv2.GaussianBlur(image,(0,0), scale / 30), -4, 128)
  
  
 	#on enleve 10% des bordures
-	b=np.zeros(image.shape)
+	b = np.zeros(image.shape)
 
-	cv2.circle(b,(int(image.shape[1]/2), int(image.shape[0]/2)),int(scale * 0.9),(1,1,1),-1,8,0)
+	cv2.circle(b, (int(image.shape[1] / 2), int(image.shape[0] / 2)), int(scale * 0.9), (1, 1, 1), -1, 8, 0)
 	#image=image*b+128*(1-b)
 	
-	image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	image=Image.fromarray (image)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+	image = Image.fromarray(image)
 	
 	return image
 
@@ -107,7 +106,7 @@ def to_grayscale(image):
 	#suppression des bordures
 	# passage en noir et blanc
 	#image=np.asarray (image)
-	image=ImageOps.grayscale(image)
+	image = ImageOps.grayscale(image)
 	#image=Image.fromarray (image)
 	return image
 
@@ -165,7 +164,7 @@ def getDiagnostic(text, diagnostic_keyphrases):
 				l.append(key)
 	return l
 
-def generateCSV(data_path, csv_name, discardNoLabels = False, discardMoreOneLabel = False, filters = [], diagnostic_keyphrases = diagnostic_keyphrases):
+def generateCSV(data_path, csv_name, discard_no_labels = True, discard_more_one_label = True, filters = [], diagnostic_keyphrases = diagnostic_keyphrases):
 	filetarget = "../data/ODIR-5K/csv/"
 	if not os.path.exists(filetarget):
 		os.makedirs(filetarget)
@@ -175,8 +174,8 @@ def generateCSV(data_path, csv_name, discardNoLabels = False, discardMoreOneLabe
 	for i, row in data.iterrows():
 		new_row = [row['Patient Age'], row['Patient Sex'], row['Left-Fundus']]
 		labels = getDiagnostic(row['Left-Diagnostic Keywords'], diagnostic_keyphrases)
-		new_row.append(labels)
-		if (not discardNoLabels or len(labels) > 0) and (not discardMoreOneLabel or len(labels) <= 1):
+		if (not discard_no_labels or len(labels) > 0) and (not discard_more_one_label or len(labels) <= 1):
+			new_row.append(labels[0])
 			if len(filters) == 0:
 				new_data.append(new_row)
 			else:
@@ -187,8 +186,8 @@ def generateCSV(data_path, csv_name, discardNoLabels = False, discardMoreOneLabe
 			
 		new_row = [row['Patient Age'], row['Patient Sex'], row['Right-Fundus']]
 		labels = getDiagnostic(row['Right-Diagnostic Keywords'], diagnostic_keyphrases)
-		new_row.append(labels)
-		if (not discardNoLabels or len(labels) > 0) and (not discardMoreOneLabel or len(labels) <= 1):
+		if (not discard_no_labels or len(labels) > 0) and (not discard_more_one_label or len(labels) <= 1):
+			new_row.append(labels[0])
 			if len(filters) == 0:
 				new_data.append(new_row)
 			else:
@@ -200,6 +199,7 @@ def generateCSV(data_path, csv_name, discardNoLabels = False, discardMoreOneLabe
 	key_columns = ['Patient Age','Patient Sex', 'Image', 'Label'] 
 
 	df = pd.DataFrame(new_data)
+	print(filetarget+csv_name)
 	df.to_csv(filetarget+csv_name, header=key_columns, index=False, encoding='utf-8')
 
 def createPreprocessingFile(filepath, filetarget, prepro):
@@ -214,30 +214,30 @@ def createPreprocessingFile(filepath, filetarget, prepro):
 	#vérifie si le fichier target existe si non le crée
 	if not os.path.exists(filetarget):
 		os.makedirs(filetarget)
+	nbFiles = len(filename)
+	cpt = 0
 	for img in filename:
 		path = os.path.join(filepath,img)
 		image = Image.open(path)
-		image = crop(image)
-		image = prepro(image)
-		image = resize(image)
+		for f in prepro:
+			image = f(image)
 		path = os.path.join(filetarget,img)
 		image.save(os.path.abspath(path))
-	print("End Prepro Writing")
+		cpt += 1
+		print(" [" + "#"*int(cpt/nbFiles * 20) + " "*(20-int(cpt/nbFiles * 20)) + "] ", end='')
+		print("%.2f"%(cpt/nbFiles * 100) + "%", end='\r')
+	print("\nEnd Prepro Writing")
 
 
 if __name__=="__main__":
 	#Le code ci-dessous devrait vous générer une série de csv et/ou dataset preprocessé
 	data_path = "../data/ODIR-5K/ODIR-5K/data.xlsx"
 	filetarget = "../data/ODIR-5K/csv/" #ne pas modifier le dossier sera créé automatiquement
-	if not os.path.exists(filetarget + "full_prepro.csv"):
-		generateCSV(data_path, "full_prepro.csv", discardNoLabels = True, discardMoreOneLabel = True)
-	if not os.path.exists(filetarget + "normale_vs_diabetic.csv"):
-		generateCSV(data_path, "normale_vs_diabetic.csv", discardNoLabels = True, discardMoreOneLabel = True, filters = [0,1])
-	if not os.path.exists(filetarget + "normale_vs_diabetic.csv"):
-		generateCSV(data_path, "normale_vs_malade.csv", discardNoLabels = True, discardMoreOneLabel = True, diagnostic_keyphrases = diagnostic_normale_or_not)
+	generateCSV(data_path, "full_prepro.csv")
+	generateCSV(data_path, "normale_vs_diabetic.csv", filters = [0,1])
+	generateCSV(data_path, "normale_vs_malade.csv", diagnostic_keyphrases = diagnostic_normale_or_not)
 
 	#potentiellement changer filepaht, vous pouvez laisser filetarget
 	filepath =  "../data/ODIR-5K/ODIR-5K/Training Images"
 	filetarget = "../data/ODIR-5K/ODIR-5K/preprocess_graham"
-	if os.path.getsize(filetarget) == 0:
-		createPreprocessingFile(filepath, filetarget, graham)
+	createPreprocessingFile(filepath, filetarget, [crop, resize, graham])
